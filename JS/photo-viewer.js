c668c0a70dbae0ece69f4b6f9c2cb8a4e38d2397
lgass95 <91688381+lgass95@users.noreply.github.com>
2022-12-07 19:13:58 -0500

    commit

--- JS/photo-viewer.js
+++ JS/photo-viewer.js
@@ -1,6 +1,12 @@
 
-function imgEnlarge(imgs) {
-    let selectedImg = document.getElementById("openImage");
-    selectedImg.src = imgs.src;
-  } 
-  
+ const img = document.getElementsByClassName('thumb');
+ for(i=0;i<img.length;i++){
+ img[i].addEventListener('click', function(e){
+         e.preventDefault();
+     })
+}
+
+ function imgEnlarge(imgs) {
+ let selectedImg = document.getElementById("openImage");
+ selectedImg.src = imgs.src;
+} 
