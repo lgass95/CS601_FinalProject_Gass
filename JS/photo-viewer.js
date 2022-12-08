10644e5fd5988bcea118f9aa5fd6b465d86bcfa1
lgass95 <91688381+lgass95@users.noreply.github.com>
2022-12-08 16:32:19 -0500

    commit

--- JS/photo-viewer.js
+++ JS/photo-viewer.js
@@ -1,6 +1,11 @@
+//unhide thumbnails element
+document.addEventListener("DOMContentLoaded", function(){
+    let thumbnails = document.getElementById('thumbnails');
+    thumbnails.style.display = 'block';
+});
 
- 
- //array loop to deactivate clickable links of thumbnails
+
+ //array loop to deactivate clickable links of thumbnails and reveal photo-viewer
  const img = document.getElementsByClassName('thumb');
  for(i=0;i<img.length;i++){
  img[i].addEventListener('click', function(e){
@@ -10,12 +15,13 @@
      })
 }
 
+
 //function to enlarge thumbnails and place them in photo-viewer
 function imgEnlarge(imgs) {
     let selectedImg = document.getElementById("openImage");
     selectedImg.src = imgs.src;
    } 
-   
+
 //function to make photo-viewer disappear after being clicked
 let view = document.getElementById('photo-viewer');
 view.addEventListener('click', function(){
