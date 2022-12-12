d6ea5be1d426ae7886b6189dbc625507db62ad13
lgass95 <91688381+lgass95@users.noreply.github.com>
2022-12-11 19:55:47 -0500

    commit

--- ArraySort/arraySort.js
+++ ArraySort/arraySort.js
@@ -1,25 +1,30 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const list = document.createElement("ul");
-    let item1 = document.createElement("li");
-    document.getElementById("list_container").appendChild(list);
-    let content = document.createTextNode("My List");
-    list.appendChild(content);
-});
-{
+    
     let words = [];
-
+   
     while(true){
-        let input = prompt ("Add a word (enter \"q\" to finish");
+        let input = prompt ("If you enter a series of words, this program will convert them to an array and sort them alphabetically. Try it out! (Enter \"q\" to finish).");
+        let word = /[A-Za-z]/.test(input);
         if (input === "q" || input === null) {
             break;
-        }
+        }else if (!word) {
+        alert("Words must only contain alphabetical letters.")
+        }else{
         words.push(input); 
         console.log(words);
+        }
     }
     let sortedWords = words.sort();
 
     console.log(sortedWords);
 
-    
-    
-}
+    let list = this.getElementById("wordList");
+    words.forEach((word) => {
+        let li = document.createElement("li");
+        li.innerText = word;
+        list.appendChild(li);
+    })
+    document.getElementById("reset").addEventListener("click", function(){
+        location.reload();
+    });
+});
